f346924d509af490c542bcc018c71e38854b8d90
SyauqiAli <sauqi2019@gmail.com>
2024-11-04 06:37:47 +0000

    changing some route code

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', fn () => to_route('landing.index') );
 
 Auth::routes();
 
