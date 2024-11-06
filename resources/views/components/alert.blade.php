bcf0dc240e42d66c945ccd48c056666154dabe47
SyauqiAli <sauqi2019@gmail.com>
2024-11-06 01:26:02 +0000

    disable alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -4,7 +4,7 @@
 
 @push('contents')
   <div class="alert-container">
-    <div class="alert alert-success" role="alert">
+    {{-- <div class="alert alert-success" role="alert">
       <svg class="icon-success" stroke="currentColor" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M13 16h-1v-4h1m0-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" stroke-width="2" stroke-linejoin="round"
           stroke-linecap="round"></path>
@@ -35,7 +35,7 @@
           stroke-linecap="round"></path>
       </svg>
       <p class="mt-2 mb-2">Error - Something went wrong.</p>
-    </div>
+    </div> --}}
   </div>
 @endpush
 
