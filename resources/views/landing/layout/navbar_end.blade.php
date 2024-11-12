aea344146552a387a45097b32f93a05de396d94a
cintaadenia <cintaadenia238@gmail.com>
2024-11-12 16:06:55 +0700

    Navbar

--- resources/views/landing/layout/navbar_end.blade.php
+++ resources/views/landing/layout/navbar_end.blade.php
@@ -1,56 +1,82 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white osahan-nav-mid px-0 border-top shadow-sm">
-  <div class="container">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-      data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/aboutUs">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            Jobs
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownPortfolio">
-            <a class="dropdown-item" href="/jobs">Jobs 1</a>
-            <a class="dropdown-item" href="/profile">Profile</a>
-            {{-- <a class="dropdown-item" href="product-detail.html">List Detail</a>
-                        <a class="dropdown-item" href="profile.html">Profile</a>
-                        <a class="dropdown-item" href="company-profile.html">Company Profile</a>
-                        <a class="dropdown-item" href="alerts.html">Alerts</a>
-                        <a class="dropdown-item" href="messages.html">Messages</a> --}}
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/chat">Chat</a>
-        </li>
-        {{-- <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        Other Pages
+    <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <style>
+            .navbar-nav {
+                display: flex;
+                flex-wrap: nowrap; /* Mencegah elemen turun ke baris berikutnya */
+                white-space: nowrap; /* Membuat teks tetap satu baris */
+                -webkit-overflow-scrolling: touch;
+            }
+
+            .nav-item {
+                margin-right: -10px; /* Jarak antar item */
+            }
+
+            @media (max-width: 768px) {
+                .navbar-nav {
+                    flex-direction: column;
+                    align-items: start;
+                }
+            }
+
+        </style>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/aboutUs">Graphic & Design</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Programing & Tech</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Video & Animation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Music & Audio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Writing & Translation</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Business</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Photography</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Finance</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logo Design</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Show More <i class="fas fa-chevron-right"></i>
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownBlog">
-                        <a class="dropdown-item" href="login.html">Login</a>
-                        <a class="dropdown-item" href="register.html">Register</a>
-                        <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
-                        <a class="dropdown-item" href="faq.html">FAQ</a>
-                        <a class="dropdown-item" href="404.html">404 Page</a>
-                        <a class="dropdown-item" href="blank.html">Blank Page</a>
+                </li>
+
+                {{-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Jobs
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownPortfolio">
+                        <a class="dropdown-item" href="/jobs">Jobs 1</a>
+                        <a class="dropdown-item" href="/profile">Profile</a>
                     </div>
                 </li> --}}
-      </ul>
-    </div>
-    <ul class="navbar-nav ml-auto">
-      {{-- <li class="nav-item">
+                {{-- <li class="nav-item">
+                    <a class="nav-link" href="/chat">Chat</a>
+                </li> --}}
+            </ul>
+        </div>
+        <ul class="navbar-nav ml-auto">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="become-a-seller.html">
                     <i class="fa fa-fw fa-trophy"></i>
                     <span>Become A Seller</span></a>
@@ -61,16 +87,16 @@
                     <span>English</span>
                 </a>
             </li> --}}
-      @guest
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('login') }}">
-            <span>Login</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('register') }}">
-            <span>Register</span></a>
-        </li>
-      @endguest
-    </ul>
-  </div>
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">
+                        <span>Login</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">
+                        <span>Register</span></a>
+                </li>
+            @endguest
+        </ul>
+    </div>
 </nav>
