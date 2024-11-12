89e6590eb40d23952c0cf755254b32ad8353b9a0
nugiartika <141117388+nugiartika@users.noreply.github.com>
2024-11-12 15:33:08 +0700

    tampilan banner

--- resources/views/landing/banner/index.blade.php
+++ resources/views/landing/banner/index.blade.php
@@ -1,40 +1,20 @@
-<section class="py-5 homepage-search-block text-center homepage-search-block-2 bg-dark position-relative">
+<section class="py-5 homepage-search-block bg-banner position-relative">
     <div class="container">
        <div class="row py-lg-5">
-          <div class="col-lg-8 mx-auto">
+          <div class="col-lg-6">
              <div class="homepage-search-title">
-                <h1 class="mb-3 text-shadow text-white font-weight-bold">Find The Perfect Freelance Services For Your Business</h1>
-                <h5 class="mb-5 text-shadow text-white-50 font-weight-normal">Millions of people use Maer to turn their ideas into reality.
-                </h5>
+                <h2 class="mb-3 text-shadow text-white font-weight-bold">Find The Perfect Freelance Services For Your Business</h2>
+                <h6 class="mb-5 text-shadow text-white-50 font-weight-normal">Millions of people use Maer to turn their ideas into reality.
+                </h6>
              </div>
              <div class="homepage-search-form">
                 <form class="form-noborder">
                    <div class="form-row">
-                      <div class="col-lg-3 col-md-3 col-sm-12 form-group">
-                         <div class="location-dropdown text-left">
-                            <i class="icofont-location-arrow"></i>
-                            <select class="custom-select form-control border-0 shadow-sm form-control-lg">
-                               <option> Categories </option>
-                               <option> Graphics & Design </option>
-                               <option> Digital Marketing </option>
-                               <option> Writing & Translation </option>
-                               <option> Video & Animation </option>
-                               <option> Music & Audio </option>
-                               <option> Programming & Tech </option>
-                               <option> Business </option>
-                               <option> Lifestyle </option>
-                               <option> Industries </option>
-                            </select>
-                         </div>
-                      </div>
-                      <div class="col-lg-7 col-md-7 col-sm-12 form-group">
-                         <input type="text" placeholder="Search..."
-                            class="form-control border-0 form-control-lg shadow-sm">
-                      </div>
-                      <div class="col-lg-2 col-md-2 col-sm-12 form-group">
-                         <button type="submit"
-                            class="btn btn-success btn-block btn-lg btn-gradient shadow-sm"><i
-                            class="fa fa-search"></i></button>
+                      <div class="col-lg-9 col-md-9 col-sm-12 form-group">
+                         <button type="button"
+                            class="form-control btn bg-white border-0 rounded-pill font-weight-bold"
+                            {{-- class="form-control border-0 form-control-lg shadow-sm rounded-pill" --}}
+                            >look for a job now</button>
                       </div>
                    </div>
                 </form>
