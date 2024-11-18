8e6db358fdad98e6b19635e5a7097746f9d533de
cintaadenia <cintaadenia238@gmail.com>
2024-11-18 11:54:19 +0700

    about-landing

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+@extends('landing.layout.app')
+@section('style')
+<style>
+    .homepage-search-title h6 {
+    max-width: 90%;
+    text-align: left;
+    white-space: normal;
+    overflow-wrap: break-word;
+}
+
+</style>
+@endsection
+@section('content')
+
+@include('landing.about')
+
+@endsection
