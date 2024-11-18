ede8ff2a175941aae334eb0e8746b43f8fa5aadf
cintaadenia <cintaadenia238@gmail.com>
2024-11-18 10:57:19 +0700

    banner landing

--- resources/views/home/layout/navbar_end.blade.php
+++ resources/views/home/layout/navbar_end.blade.php
@@ -1,26 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white osahan-nav-mid px-0 border-top shadow-sm">
-  <div class="container">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-      data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <style>
-      .navbar-nav {
-        display: flex;
-        flex-wrap: nowrap;
-        /* Mencegah elemen turun ke baris berikutnya */
-        white-space: nowrap;
-        /* Membuat teks tetap satu baris */
-        -webkit-overflow-scrolling: touch;
-        margin-left: 5px;
-      }
+    <div class="container">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <style>
+            .navbar-nav {
+                display: flex;
+                flex-wrap: nowrap;
+                /* Mencegah elemen turun ke baris berikutnya */
+                white-space: nowrap;
+                /* Membuat teks tetap satu baris */
+                -webkit-overflow-scrolling: touch;
+                margin-left: 5px;
+            }
 
-      .nav-item {
-        margin-right: 6px;
-        /* Jarak antar item */
-
-      }
-
+            .nav-item {
+                margin-right: 6px;
+                /* Jarak antar item */
+            }
         </style>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
@@ -56,44 +55,19 @@
                         Show More <i class="fas fa-chevron-right"></i>
                     </a>
                 </li>
-
-        {{-- <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Jobs
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownPortfolio">
-                        <a class="dropdown-item" href="/jobs">Jobs 1</a>
-                        <a class="dropdown-item" href="/profile">Profile</a>
-                    </div>
-                </li> --}}
-        {{-- <li class="nav-item">
-                    <a class="nav-link" href="/chat">Chat</a>
-                </li> --}}
-      </ul>
+            </ul>
+        </div>
+        <ul class="navbar-nav ml-auto">
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">
+                        <span>Login</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">
+                        <span>Register</span></a>
+                </li>
+            @endguest
+        </ul>
     </div>
-    <ul class="navbar-nav ml-auto">
-      {{-- <li class="nav-item">
-                <a class="nav-link" href="become-a-seller.html">
-                    <i class="fa fa-fw fa-trophy"></i>
-                    <span>Become A Seller</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link">
-                    <img class="country-flag img-fluid" src="{{ asset('assets_landing/images/flag/india.png') }}">
-                    <span>English</span>
-                </a>
-            </li> --}}
-      @guest
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('login') }}">
-            <span>Login</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('register') }}">
-            <span>Register</span></a>
-        </li>
-      @endguest
-    </ul>
-  </div>
 </nav>
