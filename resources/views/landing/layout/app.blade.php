5156cd01fda9974c73032270f3b4837395ded10a
SyauqiAli <sauqi2019@gmail.com>
2024-11-18 08:28:25 +0000

    dark mode

--- resources/views/landing/layout/app.blade.php
+++ resources/views/landing/layout/app.blade.php
@@ -31,16 +31,15 @@
     <link href="{{ asset('assets_landing/css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-    <link rel="stylesheet" href="{{ asset('assets_landing/css/test/style.css')}}" />
-    <link href="{{ asset('assets_landing/css/test/index.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('assets_landing/css/dark.css') }}">
 
     @yield('style')
     @stack('links')
   </head>
 
   <body>
-      @include('landing.layout.navbar')
-    
+    @include('landing.layout.navbar')
+
     @yield('content')
     @stack('contents')
 
