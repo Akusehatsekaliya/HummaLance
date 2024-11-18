81c7efeeacc9293abc0ac351a0c97a7607681ff0
nugiartika <141117388+nugiartika@users.noreply.github.com>
2024-11-18 13:28:00 +0700

    about-landing

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -1,31 +1,26 @@
 <style>
-    .custom-title {
-        line-height: 1.50px;
-        /* Mengatur tinggi baris */
-        margin-top: 14px;
-        /* Mengatur jarak atas */
-        margin-bottom: 50px;
-        /* Mengatur jarak bawah */
-        margin-left: 12px;
-    }
+    .homepage-search-block{
+        height: 50vh;
+        }
 </style>
-<section class="py-5 homepage-search-block bg-landing position-relative">
+
+<section class="py-5 homepage-search-block bg-landing bg-about position-relative">
     <div class="container">
-        <div class="row justify-content-center d-flex align-items-center">
-            <div class="col-lg-4 text-center" style="margin-top: -30px; margin-bottom: -80px;">
-                <div class="homepage">
-                    <h6 style="color: white; font-size: 20px;">About Us</h6>
-                </div>
-                <p style="color: rgb(255, 255, 255); font-size: 12px;">
-                    Home
-                    <svg width="12" height="12" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
+        <div class="row py-lg-5 justify-content-center d-flex align-items-center">
+                <div class="homepage mt-5 text-center">
+                    <h3 class="text-white font-weight-bold">About Us</h3>
+                    <p class="text-white">
+                        Home
+                        <svg width="12" height="12" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
                             d="M7.76469 7.00195L2.61237 1.61133C2.3001 1.33789 1.96833 1.33789 1.61703 1.61133C1.48042 1.74805 1.41211 1.91406 1.41211 2.10938C1.41211 2.28516 1.48042 2.45117 1.61703 2.60742L6.30096 7.5L1.61703 12.3926C1.3438 12.7441 1.3438 13.0762 1.61703 13.3887C1.96833 13.6621 2.3001 13.6621 2.61237 13.3887L7.76469 7.99805C8.03792 7.66602 8.03792 7.33398 7.76469 7.00195ZM13.3854 7.00195L8.23308 1.61133C7.92082 1.33789 7.58904 1.33789 7.23775 1.61133C7.10113 1.74805 7.03283 1.91406 7.03283 2.10938C7.03283 2.28516 7.10113 2.45117 7.23775 2.60742L11.9217 7.5L7.23775 12.3926C6.96452 12.7441 6.96452 13.0762 7.23775 13.3887C7.58904 13.6621 7.92082 13.6621 8.23308 13.3887L13.3854 7.99805C13.6586 7.66602 13.6586 7.33398 13.3854 7.00195Z"
                             fill="white" />
-                    </svg>
-                    <a href="#" style="color: rgb(255, 255, 255); text-decoration: none;">About Us</a>
-                </p>
-            </div>
+                        </svg>
+                        <a href="#" style="color: rgb(255, 255, 255); text-decoration: none;">About Us</a>
+                    </p>
+                </div>
         </div>
     </div>
 </section>
+
+
