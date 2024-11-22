5574f82685e0fc437dfbc5d7c42b634b08003efa
cintaadenia <cintaadenia238@gmail.com>
2024-11-22 10:05:08 +0700

    navbar

--- resources/views/home/layout/navbar_first.blade.php
+++ resources/views/home/layout/navbar_first.blade.php
@@ -1,11 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-light topbar static-top shadow-sm bg-white osahan-nav-top px-0">
     <div class="container">
         <!-- Sidebar Toggle (Topbar) -->
-        <a class="navbar-brand" href="#"><img src="{{ asset('assets_landing/images/Asset 15.png') }}" alt=""></a>
+        <a class="navbar-brand" href="#"><img src="{{ asset('assets_landing/images/Asset 15.png') }}"
+                alt=""></a>
         <!-- Topbar Search -->
         <form class="d-none d-sm-inline-block form-inline mr-auto mw-100 navbar-search">
             <div class="input-group" style="width: 460px;">
-                <input type="text" class="form-control bg-white small" placeholder="Find Services..." aria-label="Search" aria-describedby="basic-addon2">
+                <input type="text" class="form-control bg-white small" placeholder="Find Services..."
+                    aria-label="Search" aria-describedby="basic-addon2">
                 <div class="input-group-append">
                     <button class="btn btn-success" type="button">
                         <i class="fa fa-search fa-sm"></i>
@@ -35,6 +37,8 @@
                     </li>
                 </ul>
             </div>
+
+
             <ul class="navbar-nav align-items-center ml-auto">
                 <li class="nav-item dropdown no-arrow no-caret mr-3 dropdown-notifications d-sm-none">
                     <a class="btn btn-icon btn-transparent-dark dropdown-toggle" href="#" id="searchDropdown"
@@ -58,97 +62,86 @@
                     </div>
                 </li>
 
-                {{-- notifications --}}
+                {{-- Notification --}}
                 <li class="nav-item dropdown no-arrow no-caret mr-3 dropdown-notifications">
-                    <div class="icon">
-                        <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownAlerts"
-                            href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bell"
-                                class="svg-inline--fa fa-bell fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 448 512">
-                                <path fill="currentColor"
-                                    d="M224 512c35.3 0 64-28.7 64-64H160c0 35.3 28.7 64 64 64zm215.4-149.3c-20.9-21.5-55.4-53.6-55.4-154.7 0-77.7-54.5-139.2-127.9-155.2V32c0-17.7-14.3-32-32-32s-32 14.3-32 32v20.8C118.5 69.8 64 131.3 64 209c0 101.1-34.5 133.2-55.4 154.7-6 6.2-8.6 14.1-8.6 22.3 0 17.4 13.9 32 32 32h384c18.1 0 32-14.6 32-32 0-8.2-2.6-16.1-8.6-22.3z">
-                                </path>
+                    <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownAlerts"
+                        href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-bell">
+                            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                            <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+                        </svg>
+                    </a>
+                    <div class="notification-badge">81</div>
+                    <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up"
+                        aria-labelledby="navbarDropdownAlerts">
+                        <h6 class="dropdown-header dropdown-notifications-header">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-bell mr-2">
+                                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                                <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
                             </svg>
-                            <div class="notification-badge">8</div>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up"
-                            aria-labelledby="navbarDropdownAlerts">
-                            <h6 class="dropdown-header dropdown-notifications-header">
+                            Alerts Center
+                        </h6>
+                        <a class="dropdown-item dropdown-notifications-item" href="#!">
+                            <div class="dropdown-notifications-item-icon bg-warning">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" class="feather feather-bell mr-2">
-                                    <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-                                    <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+                                    stroke-linejoin="round" class="feather feather-activity">
+                                    <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
                                 </svg>
-                                Notifications
-                            </h6>
-                            <a class="dropdown-item dropdown-notifications-item" href="#!">
-                                <div class="dropdown-notifications-item-icon bg-danger">
-                                    <svg class="svg-inline--fa fa-exclamation-triangle fa-w-18" aria-hidden="true"
-                                        focusable="false" data-prefix="fas" data-icon="exclamation-triangle"
-                                        role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"
-                                        data-fa-i2svg="">
-                                        <path fill="currentColor"
-                                            d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z">
-                                        </path>
-                                    </svg>
-                                    <!-- <i class="fas fa-exclamation-triangle"></i> -->
-                                </div>
-                                <div class="dropdown-notifications-item-content">
-                                    <div class="dropdown-notifications-item-content-details">December 8, 2020</div>
-                                    <div class="dropdown-notifications-item-content-text">Critical system failure, systems
-                                        shutting down.</div>
-                                </div>
-                            </a>
-                            {{-- <a class="dropdown-item dropdown-notifications-footer" href="alerts.html">View All Alerts</a> --}}
-                        </div>
+                            </div>
+                            <div class="dropdown-notifications-item-content">
+                                <div class="dropdown-notifications-item-content-details">December 29, 2020</div>
+                                <div class="dropdown-notifications-item-content-text">This is an alert message. It's
+                                    nothing serious, but it requires your attention.</div>
+                            </div>
+                        </a>
+                        <a class="dropdown-item dropdown-notifications-footer" href="alerts.html">View All Alerts</a>
                     </div>
                 </li>
 
                 {{-- email --}}
                 <li class="nav-item dropdown no-arrow no-caret mr-3 dropdown-notifications">
-                    <div class="icon">
-
-                        <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownMessages"
-                            href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope"
-                                class="svg-inline--fa fa-envelope fa-w-16" role="img"
-                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                <path fill="currentColor"
-                                    d="M502.3 190.8L327.4 338.3c-22.9 19.6-55.8 19.6-78.7 0L9.7 190.8C3.9 186.2 0 179.2 0 171.6V48c0-26.5 21.5-48 48-48h416c26.5 0 48 21.5 48 48v123.6c0 7.6-3.9 14.6-9.7 19.2zM464 64H48c-8.8 0-16 7.2-16 16v23.2l192 164.8c13.3 11.4 32.7 11.4 46 0L480 103.2V80c0-8.8-7.2-16-16-16zm-16 384H64c-35.3 0-64-28.7-64-64V192c0-7.6 3.9-14.6 9.7-19.2l218.7 187.8c22.9 19.6 55.8 19.6 78.7 0l218.7-187.8c5.8 4.6 9.7 11.6 9.7 19.2v192c0 35.3-28.7 64-64 64z">
+                    <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownMessages"
+                        href="javascript:void(0);" role="button" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-mail">
+                            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                            <polyline points="22,6 12,13 2,6"></polyline>
+                        </svg>
+                    </a>
+                    <div class="notification-badge">11</div>
+                    <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up"
+                        aria-labelledby="navbarDropdownMessages">
+                        <h6 class="dropdown-header dropdown-notifications-header">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="feather feather-mail mr-2">
+                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
                                 </path>
+                                <polyline points="22,6 12,13 2,6"></polyline>
                             </svg>
-                            <div class="notification-badge">11</div>
+                            Message Center
+                        </h6>
+                        <a class="dropdown-item dropdown-notifications-item" href="#!">
+                            <img class="dropdown-notifications-item-img" src="images/user/s7.png">
+                            <div class="dropdown-notifications-item-content">
+                                <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet,
+                                    consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                                <div class="dropdown-notifications-item-content-details">Emily Fowler · 58m</div>
+                            </div>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-right border-0 shadow animated--fade-in-up"
-                            aria-labelledby="navbarDropdownMessages">
-                            <h6 class="dropdown-header dropdown-notifications-header">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail mr-2">
-                                    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
-                                    </path>
-                                    <polyline points="22,6 12,13 2,6"></polyline>
-                                </svg>
-                                Email
-                            </h6>
-                            <a class="dropdown-item dropdown-notifications-item" href="#!">
-                                <img class="dropdown-notifications-item-img" src="images/user/s7.png">
-                                <div class="dropdown-notifications-item-content">
-                                    <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet,
-                                        consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                                        laborum.</div>
-                                    <div class="dropdown-notifications-item-content-details">Emily Fowler · 58m</div>
-                                </div>
-                            </a>
-                        </div>
+                        <a class="dropdown-item dropdown-notifications-footer" href="messages.html">Read All Messages</a>
                     </div>
                 </li>
 
--- resources/views/landing/banner.blade.php
+++ resources/views/landing/banner.blade.php
@@ -1,18 +1,17 @@
 <style>
     .custom-title {
-    line-height: 1.50px;
-    margin-top: 14px;
-    margin-bottom: 50px;
-    margin-left: 12px;
-}
-
+        line-height: 1.50px;
+        margin-top: 14px;
+        margin-bottom: 50px;
+        margin-left: 12px;
+    }
 </style>
 <section class="py-5 homepage-search-block bg-landing position-relative" style="z-index: -1;">
     <div class="container">
         <div class="row justify-content-center d-flex align-items-center">
             <div class="col-lg-7">
                 <div class="row" style="margin-right: 12rem; margin-left: unset">
-                    <x-svg>{{ asset("assets/svg/landing/banner/Start Work Today.svg") }}</x-svg>
+                    <x-svg>{{ asset('assets/svg/landing/banner/Start Work Today.svg') }}</x-svg>
                     <h6 class="custom-title mb-4 text-shadow text-white font-weight-normal">
                         Start Work Today
                     </h6>
@@ -32,12 +31,7 @@
                                 <a href="#" class="btn btn-outline-login1 text-center"
                                     style="font-size: 1rem; font-weight: bold;">
                                     Get Started
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M20.7166 13.0602C20.9975 12.7789 21.1553 12.3977 21.1553 12.0002C21.1553 11.6027 20.9975 11.2214 20.7166 10.9402L15.0606 5.28116C14.7792 4.99976 14.3975 4.84167 13.9996 4.84167C13.6016 4.84167 13.22 4.99976 12.9386 5.28116C12.6572 5.56255 12.4991 5.9442 12.4991 6.34216C12.4991 6.74011 12.6572 7.12176 12.9386 7.40315L16.0346 10.5002L4.49959 10.5002C4.10177 10.5002 3.72024 10.6582 3.43893 10.9395C3.15763 11.2208 2.99959 11.6023 2.99959 12.0002C2.99959 12.398 3.15763 12.7795 3.43893 13.0608C3.72024 13.3421 4.10177 13.5002 4.49959 13.5002L16.0346 13.5002L12.9386 16.5962C12.7993 16.7355 12.6887 16.9009 12.6133 17.0829C12.5379 17.265 12.4991 17.4601 12.4991 17.6572C12.4991 17.8542 12.5379 18.0493 12.6133 18.2314C12.6887 18.4134 12.7993 18.5788 12.9386 18.7182C13.0779 18.8575 13.2433 18.968 13.4254 19.0434C13.6074 19.1188 13.8025 19.1576 13.9996 19.1576C14.1966 19.1576 14.3918 19.1188 14.5738 19.0434C14.7558 18.968 14.9213 18.8575 15.0606 18.7182L20.7166 13.0602Z"
-                                            fill="#01ABFE" />
-                                    </svg>
+                                    <x-svg>{{ asset('assets/svg/landing/banner/getStarted.svg') }}</x-svg>
                                 </a>
 
                                 <a href="#" class="btn btn-outline-landing1 text-center ml-3"
