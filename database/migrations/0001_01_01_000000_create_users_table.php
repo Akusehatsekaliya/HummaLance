ed28dd34617371c28eea67b96b7cb7e5306a985e
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-11-22 09:57:20 +0700

    action users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            // $table->string('username');
             $table->string('name');
             $table->text('avatar')->nullable();
             $table->string('email')->unique();
@@ -31,7 +30,7 @@ return new class extends Migration
             $table->enum('status',allowed:
             [
                 UserStatusEnum::ACTIVE->value,
-                UserStatusEnum::NONACTIVE->value,
+                UserStatusEnum::BANNED->value,
             ]);
             $table->rememberToken();
             $table->timestamps();
