9f3d241f29faa97ea7caf67b382b7195bdcc0387
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-11-26 14:49:44 +0700

    contract detail and migration

--- database/migrations/2024_11_06_023329_create_contracts_table.php
+++ database/migrations/2024_11_06_023329_create_contracts_table.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Constract\Enums\ApprovalStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Constract\Enums\StatusContractEnum;
+use App\Constract\Enums\StatusPaymentEnum;
 
 return new class extends Migration
 {
@@ -16,12 +18,25 @@ return new class extends Migration
             $table->id();
             $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('amount_id')->constrained('projects')->onDelete('cascade');
             $table->enum('status', [
                 StatusContractEnum::PROGRESS->value,
                 StatusContractEnum::COMPLETE->value,
             ])->default(StatusContractEnum::PROGRESS->value);
             $table->date('start_date');
             $table->date('end_date');
+            $table->string('file')->nullable();
+            $table->text('description')->nullable();
+            $table->enum('payment_status',[
+                StatusPaymentEnum::COMPLETE->value,
+                StatusPaymentEnum::PENDING->value,
+            ])->default(StatusPaymentEnum::PENDING->value,);
+            $table->enum('approval_status',[
+                ApprovalStatusEnum::REJECTED->value,
+                ApprovalStatusEnum::PENDING->value,
+                ApprovalStatusEnum::APPROVED->value,
+            ])->default(ApprovalStatusEnum::APPROVED->value,);
+            $table->date('payment_date')->nullable();
             $table->timestamps();
         });
     }
