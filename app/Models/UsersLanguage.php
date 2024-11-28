2155d75c1cdd843750de82e1f9a699d150a48cb1
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-11-28 11:22:02 +0700

    users bahasa migration

---
+++ app/Models/UsersLanguage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsersLanguage extends Model
+{
+    protected $fillable =
+    [
+        'users_id',
+        'languages_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+
+    public function language()
+    {
+        return $this->belongsTo(Language::class, 'languages_id');
+    }
+}
