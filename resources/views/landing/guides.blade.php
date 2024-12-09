bbf2f04ad28017f8bb59eaf844cd3039f1058549
cintaadenia <cintaadenia238@gmail.com>
2024-12-09 08:27:26 +0700

    copyWriting Guides

--- resources/views/landing/guides.blade.php
+++ resources/views/landing/guides.blade.php
@@ -23,6 +23,7 @@
 
     <section class="py-5 mb-5 mt-5">
         <div class="container">
+            {{-- svg  vector--}}
             <div class="svg-container4">
                 <x-svg>{{ asset('assets/svg/landing/guides.svg') }}</x-svg>
             </div>
@@ -32,7 +33,9 @@
                     <span class="mr-1"></span>
                     Learn How To Use Clocker With Us
                 </h5>
-                <h2>Guides For <span class="text-landing">You</span></h2>
+                <h2 class="inline-heading">
+                    Guides For <span class="text-landing">Freelancer And Jobseeker</span>
+                  </h2>
             </div>
             <div class="row-fitur1">
                 <div class="fitur-item">
@@ -40,9 +43,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Self Development.svg') }}</x-svg>
 
-                            <h5>Self Development</h5>
-                            <p>By joining clocker you can develop yourself further in your work and hone your
-                                skills.</p>
+                            <h5>Start Looking For Work</h5>
+                            <p>At Clocker if you are a job seeker, you can start by looking for a job that suits you.</p>
                         </div>
                     </div>
                 </div>
@@ -51,9 +53,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Time Management.svg') }}</x-svg>
 
-                            <h5>Time Management</h5>
-                            <p>in clocker you can manage your time more efficiently with various working hours from
-                                many vacancies</p>
+                            <h5>Look For a Suitable Job</h5>
+                            <p>At Clocker, there are many types of jobs available that you can choose according to your passion and expertise.</p>
                         </div>
                     </div>
                 </div>
@@ -62,8 +63,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Career Growth.svg') }}</x-svg>
 
-                            <h5>Career Growth</h5>
-                            <p>At clocker you can grow your career more rapidly for your future</p>
+                            <h5>Create a Strong Portfolio</h5>
+                            <p>Create a strong portfolio so that it will be easier for you to find a job</p>
                         </div>
                     </div>
                 </div>
@@ -72,9 +73,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Global Networking.svg') }}</x-svg>
 
-                            <h5>Global Networking</h5>
-                            <p>Connect with clients and other professionals worldwide, opening doors to long-term
-                                partnerships.</p>
+                            <h5>Increase Connections</h5>
+                            <p>At Clocker you can expand your connections with many people so you can stay updated on job information.</p>
                         </div>
                     </div>
                 </div>
@@ -91,33 +91,32 @@
                         <div class="card-guides-item">
                             <x-svg>{{ asset('assets/svg/landing/about/card-about-item/Easy To Use.svg') }}</x-svg>
 
-                            <h5 class="title1">Easy To Use</h5>
-                            <span class="font-italic">By joining clocker you can develop yourself further in </span>
+                            <h5 class="title1">Start With Make Freelancer account</h5>
+                            <span class="font-italic">You can start your journey as a freelancer by creating a freelancer account on clocker.</span>
                         </div>
 
                         <div class="card-guides-item">
                             <x-svg>{{ asset('assets/svg/landing/about/card-about-item/Varied work.svg') }}</x-svg>
 
-                            <h5 class="title1">Varied work</h5>
-                            <span class="font-italic">By joining clocker you can develop yourself further in </span>
+                            <h5 class="title1">Start Making Services</h5>
+                            <span class="font-italic">You can start by creating services and selling your services on clocker as a freelancer</span>
                         </div>
 
                         <div class="card-guides-item">
                             <x-svg>{{ asset('assets/svg/landing/about/card-about-item/Diverse companies.svg') }}</x-svg>
 
-                            <h5 class="title1">Diverse companies</h5>
-                            <span class="font-italic">By joining clocker you can develop yourself further in </span>
+                            <h5 class="title1">Market Your Services</h5>
+                            <span class="font-italic">After creating your service, you can start marketing your service and expanding your connections on Clocker.</span>
                         </div>
 
                     </div>
                 </div>
                 <div class="col-lg-5 text-white">
-                    <h2 class="font-weight-bold">This is why you need to use a clocker</h2>
-                    <p style="font-size: 20px; font-weight:300;">At Clocker you can find many job vacancies with the working
-                        hours and skills you want</p>
+                    <h2 class="font-weight-bold">Start Your Freelancing Journey With Us</h2>
+                    <p style="font-size: 20px; font-weight:300;">At Clocker you will start your journey as a freelancer by starting to sell your services and developing your career in the freelance world.</p>
                     <a href="#" class="btn btn-outline-landing1 text-center"
                         style="font-size: 14px; font-weight: bold;">
-                        Try Clocker Now
+                        Star Your Journey Now
                         <x-svg>{{ asset('assets/svg/icons-24/icon-next.svg') }}</x-svg>
                     </a>
                 </div>
@@ -133,7 +132,7 @@
                     <span class="mr-1"></span>
                     Learn How To Use Clocker With Us
                 </h5>
-                <h2>Guides For <span class="text-landing">You</span></h2>
+                <h2>Guides For <span class="text-landing">Client And Company</span></h2>
             </div>
             <div class="row-fitur1">
                 <div class="fitur-item">
@@ -141,9 +140,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Self Development.svg') }}</x-svg>
 
-                            <h5>Self Development</h5>
-                            <p>By joining clocker you can develop yourself further in your work and hone your
-                                skills.</p>
+                            <h5>Create Job Vacancies</h5>
+                            <p>By joining Clocker as a client or company owner, you can create vacancies to find workers.</p>
                         </div>
                     </div>
                 </div>
@@ -152,9 +150,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Time Management.svg') }}</x-svg>
 
-                            <h5>Time Management</h5>
-                            <p>in clocker you can manage your time more efficiently with various working hours from
-                                many vacancies</p>
+                            <h5>Hire Freelancers</h5>
+                            <p>And you can employ expert freelancers registered with clocker by purchasing their services.</p>
                         </div>
                     </div>
                 </div>
@@ -163,8 +160,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Career Growth.svg') }}</x-svg>
 
-                            <h5>Career Growth</h5>
-                            <p>At clocker you can grow your career more rapidly for your future</p>
+                            <h5>Best Candidate</h5>
+                            <p>You can see job applicants with strong portfolios on clocker</p>
                         </div>
                     </div>
                 </div>
@@ -173,9 +170,8 @@
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Global Networking.svg') }}</x-svg>
 
-                            <h5>Global Networking</h5>
-                            <p>Connect with clients and other professionals worldwide, opening doors to long-term
-                                partnerships.</p>
+                            <h5>Best Freelancer</h5>
+                            <p>At Clocker you can find the best freelancers easily, because we always display the best freelancers every month.</p>
                         </div>
                     </div>
                 </div>
@@ -213,49 +209,6 @@
                 </div>
                 <div class="fitur-item">
                     <div class="card-fitur-item3">
-                        <div class="main-fitur">
-                            <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Global Networking.svg') }}</x-svg>
-
-                            <h5>Global Networking</h5>
-                            <p>Connect with clients and other professionals worldwide, opening doors to long-term
-                                partnerships.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="fitur-item">
-                    <div class="card-fitur-item3">
-                        <div class="main-fitur">
-                            <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Self Development.svg') }}</x-svg>
-
-                            <h5>Self Development</h5>
-                            <p>By joining clocker you can develop yourself further in your work and hone your
-                                skills.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="fitur-item">
-                    <div class="card-fitur-item3">
-                        <div class="main-fitur">
-                            <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Time Management.svg') }}</x-svg>
-
-                            <h5>Time Management</h5>
-                            <p>in clocker you can manage your time more efficiently with various working hours from
-                                many vacancies</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="fitur-item">
-                    <div class="card-fitur-item3">
-                        <div class="main-fitur">
-                            <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Career Growth.svg') }}</x-svg>
-
-                            <h5>Career Growth</h5>
-                            <p>At clocker you can grow your career more rapidly for your future</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="fitur-item">
-                    <div class="card-fitur-item3 ">
                         <div class="main-fitur">
                             <x-svg>{{ asset('assets/svg/landing/about/main-fitur/Global Networking.svg') }}</x-svg>
 
