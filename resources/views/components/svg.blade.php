89d86f54508501be93423ba230b6edbd3ef25209
SyauqiAli <sauqi2019@gmail.com>
2024-12-10 03:05:37 +0000

    fix svg

--- resources/views/components/svg.blade.php
+++ resources/views/components/svg.blade.php
@@ -1,9 +1,14 @@
 @php
-  $filePath = public_path(str_replace(url('/'), '', $slot));
+    $parsedUrl = parse_url($slot);
+    $path = $parsedUrl['path'] ?? $slot;
+
+    $path = ltrim($path, '/');
+
+    $filePath = public_path($path);
 @endphp
 
 @if (file_exists($filePath))
-  {!! file_get_contents($filePath) !!}
+    {!! file_get_contents($filePath) !!}
 @else
-  <img src="" alt="{{ $slot }}">
+    <img src="{{ $slot }}" alt="{{ basename($path) }}">
 @endif
