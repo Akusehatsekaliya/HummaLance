aa8faf7501280d0a02c3b3f5263713ba42702c4e
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-12-11 13:38:08 +0700

    route forgot dan reset passsword

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\Auth\RegisterController;
 
 // use App\Http\Controllers\Auth\SetPasswordControllerSetPasswordController;
@@ -11,9 +13,11 @@ Auth::routes();
 
 Route::get("login", fn() => redirect(route('landing.index') . '#login'))->name("login");
 Route::get("register", fn() => redirect(route('landing.index') . '#register'))->name("register");
+// register freelancer
 Route::get('/freelancer-register', [RegisterController::class, 'RegisterFreelancerStore'])->name('freelancer_register');
-Route::get('/freelancer-register', [RegisterController::class, 'RegisterFreelancerGoogleStore'])->name('freelancer_register_google');
-Route::get('/freelancer-register', [RegisterController::class, 'RegisterFreelancerStoreNextGoogle'])->name('freelancer_register_next_google');
+Route::get('/freelancer-register-google', [RegisterController::class, 'RegisterFreelancerGoogleStore'])->name('freelancer_register_google');
+Route::get('/freelancer-register-google-next', [RegisterController::class, 'RegisterFreelancerStoreNextGoogle'])->name('freelancer_register_next_google');
+// end
 Route::get('/register', function () {
     return view('auth.option');
 })->name('option');
@@ -30,3 +34,15 @@ Route::get('/optionExperience', function () {
 // Google Login
 Route::get('login/google', [LoginController::class, 'redirectToGoogle'])->name('login.google');
 Route::get('login/google/callback', [LoginController::class, 'handleGoogleCallback']);
+
+// Menampilkan form "Lupa Password"
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+
+// Mengirim email link reset password
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+// Menampilkan form reset password berdasarkan token
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+
+// Memproses reset password
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
