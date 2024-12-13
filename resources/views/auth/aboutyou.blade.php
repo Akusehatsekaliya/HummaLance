e2c60868fdd284cd015a33ca2de02b9009a19a89
cintaadenia <cintaadenia238@gmail.com>
2024-12-13 15:46:55 +0700

    modal upload add your resume

--- resources/views/auth/aboutyou.blade.php
+++ resources/views/auth/aboutyou.blade.php
@@ -34,10 +34,6 @@
             color: #007bff;
         }
 
-        /* .footer button:hover {
-                    background-color: #f0f0f0;
-                } */
-
         .shift-right {
             margin-left: 45px;
         }
@@ -48,7 +44,6 @@
             margin: 0;
             margin-left: 45px;
         }
-
 
         .progress-container {
             position: absolute;
@@ -115,6 +110,7 @@
         .img-bg {
             width: 100%
         }
+
         .img-profile {
             width: 100px;
             position: absolute;
@@ -131,28 +127,96 @@
         ul li {
             list-style: inside;
         }
+
         .btn-outline-custom {
-        border: 2px solid #00AAFF;
-        width: 100%;
-        color: #00AAFF;
-        border-radius: 12px;
-        padding: 10px;
-        box-shadow: 0 0.5px 2px #aaaaaa;
-    }
+            border: 2px solid #00AAFF;
+            width: 100%;
+            color: #00AAFF;
+            border-radius: 12px;
+            padding: 10px;
+            box-shadow: 0 0.5px 2px #aaaaaa;
+        }
 
-    .btn-outline-custom:hover {
-        color: #00AAFF;
-    }
-    .img-about{
-        height: 153px;
-    }
+        .btn-outline-custom:hover {
+            color: #00AAFF;
+        }
 
-    .modal-content {
-        border-radius: 10px;
-    }
-    .border-header{
-        border-bottom: none;
-    }
+        .img-about {
+            height: 153px;
+        }
+
+        .modal-content {
+            border-radius: 10px;
+        }
+
+        .border-header {
+            border-bottom: none;
+        }
+
+        .close-btn {
+            position: absolute;
+            top: 10px;
+            right: 35px;
+            font-size: 40px;
+            cursor: pointer;
+        }
+
+        h1 {
+            font-size: 24px;
+            color: #333;
+        }
+
+        p {
+            font-size: 14px;
+            color: #666;
+        }
+
+        .upload-box {
+            border: 2px dashed #666;
+            /* border-radius: 8px; */
+            padding: 40px;
+            margin: 20px 0;
+            background-color: #21bde828;
+            position: relative;
+        }
+
+        .upload-box img {
+            width: 100px;
+            height: auto;
+        }
+
+        .upload-box p {
+            margin: 25px 0 0;
+            color: #666;
+        }
+
+        .upload-box a {
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        .upload-box a:hover {
+            text-decoration: underline;
+        }
+
+        .continue-btn {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        .continue-btn:hover {
+            background-color: #0056b3;
+        }
+
+        .modal-footer {
+            margin-top: 55px;
+            border-top: none;
+        }
     </style>
 @endsection
 @section('content')
@@ -166,8 +230,7 @@
             <div class="px-4">
                 <h4 class="mt-3 font-weight-bold">Ahmad Rifaldi K.</h4>
                 <div class="row px-3 text-secondary">
-                    <h5 class="mr-2"> Graphic Designer</h5>
-                    <h5>(He/Him)</h5>
+                    <h6 class="mr-2" style="margin-left: -17px;"> Graphic Designer (He/Him)</h6>
                 </div>
                 <div class="text-secondary">
                     <p class="text-justify mb-2">I am a graphic designer who is experienced in creating design feeds for
@@ -194,58 +257,79 @@
     <div class="col-lg-4 pb-4">
         <span style="font-weight: 600;">About You</span>
         <h4 class="font-weight-bold my-3">Can you tell us about yourself?</h4>
-        <p>Everyone has a different version of themselves, and we want to know your story so we can recommend jobs that are a good fit for you.</p>
+        <p>Everyone has a different version of themselves, and we want to know your story so we can recommend jobs that are
+            a good fit for you.</p>
 
         <div class="button-about">
             <button class="btn btn-outline-custom my-3">
-                <svg width="25" class="mr-2" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="25" class="mr-2" height="25" viewBox="0 0 25 25" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
                     <g clip-path="url(#clip0_1594_1033)">
-                    <path d="M22.2222 0C22.9589 0 23.6655 0.292658 24.1864 0.813592C24.7073 1.33453 25 2.04107 25 2.77778V22.2222C25 22.9589 24.7073 23.6655 24.1864 24.1864C23.6655 24.7073 22.9589 25 22.2222 25H2.77778C2.04107 25 1.33453 24.7073 0.813592 24.1864C0.292658 23.6655 0 22.9589 0 22.2222V2.77778C0 2.04107 0.292658 1.33453 0.813592 0.813592C1.33453 0.292658 2.04107 0 2.77778 0H22.2222ZM21.5278 21.5278V14.1667C21.5278 12.9658 21.0507 11.8142 20.2016 10.965C19.3525 10.1159 18.2008 9.63889 17 9.63889C15.8194 9.63889 14.4444 10.3611 13.7778 11.4444V9.90278H9.90278V21.5278H13.7778V14.6806C13.7778 13.6111 14.6389 12.7361 15.7083 12.7361C16.224 12.7361 16.7186 12.941 17.0833 13.3056C17.4479 13.6703 17.6528 14.1649 17.6528 14.6806V21.5278H21.5278ZM5.38889 7.72222C6.00773 7.72222 6.60122 7.47639 7.03881 7.03881C7.47639 6.60122 7.72222 6.00773 7.72222 5.38889C7.72222 4.09722 6.68056 3.04167 5.38889 3.04167C4.76637 3.04167 4.16934 3.28896 3.72915 3.72915C3.28896 4.16934 3.04167 4.76637 3.04167 5.38889C3.04167 6.68056 4.09722 7.72222 5.38889 7.72222ZM7.31944 21.5278V9.90278H3.47222V21.5278H7.31944Z" fill="#00AAFF"/>
+                        <path
+                            d="M22.2222 0C22.9589 0 23.6655 0.292658 24.1864 0.813592C24.7073 1.33453 25 2.04107 25 2.77778V22.2222C25 22.9589 24.7073 23.6655 24.1864 24.1864C23.6655 24.7073 22.9589 25 22.2222 25H2.77778C2.04107 25 1.33453 24.7073 0.813592 24.1864C0.292658 23.6655 0 22.9589 0 22.2222V2.77778C0 2.04107 0.292658 1.33453 0.813592 0.813592C1.33453 0.292658 2.04107 0 2.77778 0H22.2222ZM21.5278 21.5278V14.1667C21.5278 12.9658 21.0507 11.8142 20.2016 10.965C19.3525 10.1159 18.2008 9.63889 17 9.63889C15.8194 9.63889 14.4444 10.3611 13.7778 11.4444V9.90278H9.90278V21.5278H13.7778V14.6806C13.7778 13.6111 14.6389 12.7361 15.7083 12.7361C16.224 12.7361 16.7186 12.941 17.0833 13.3056C17.4479 13.6703 17.6528 14.1649 17.6528 14.6806V21.5278H21.5278ZM5.38889 7.72222C6.00773 7.72222 6.60122 7.47639 7.03881 7.03881C7.47639 6.60122 7.72222 6.00773 7.72222 5.38889C7.72222 4.09722 6.68056 3.04167 5.38889 3.04167C4.76637 3.04167 4.16934 3.28896 3.72915 3.72915C3.28896 4.16934 3.04167 4.76637 3.04167 5.38889C3.04167 6.68056 4.09722 7.72222 5.38889 7.72222ZM7.31944 21.5278V9.90278H3.47222V21.5278H7.31944Z"
+                            fill="#00AAFF" />
                     </g>
                     <defs>
-                    <clipPath id="clip0_1594_1033">
-                    <rect width="25" height="25" fill="white"/>
-                    </clipPath>
+                        <clipPath id="clip0_1594_1033">
+                            <rect width="25" height="25" fill="white" />
+                        </clipPath>
                     </defs>
                 </svg>
 
                 Impor Form Linkedin
             </button>
-            <button class="btn btn-outline-custom my-3"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                <svg width="20" class="mr-2" height="25" viewBox="0 0 20 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <button class="btn btn-outline-custom my-3" type="button" class="btn btn-primary" data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop">
+                <svg width="20" class="mr-2" height="25" viewBox="0 0 20 25" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
                     <g clip-path="url(#clip0_1594_1042)">
-                    <path d="M11.25 8.75H18.125L11.25 1.875V8.75ZM2.5 0H12.5L20 7.5V22.5C20 23.163 19.7366 23.7989 19.2678 24.2678C18.7989 24.7366 18.163 25 17.5 25H2.5C1.83696 25 1.20107 24.7366 0.732233 24.2678C0.263392 23.7989 0 23.163 0 22.5V2.5C0 1.1125 1.1125 0 2.5 0ZM12.5 22.5V21.25C12.5 19.5875 9.1625 18.75 7.5 18.75C5.8375 18.75 2.5 19.5875 2.5 21.25V22.5H12.5ZM7.5 12.5C6.83696 12.5 6.20107 12.7634 5.73223 13.2322C5.26339 13.7011 5 14.337 5 15C5 15.663 5.26339 16.2989 5.73223 16.7678C6.20107 17.2366 6.83696 17.5 7.5 17.5C8.16304 17.5 8.79893 17.2366 9.26777 16.7678C9.73661 16.2989 10 15.663 10 15C10 14.337 9.73661 13.7011 9.26777 13.2322C8.79893 12.7634 8.16304 12.5 7.5 12.5Z" fill="#00AAFF"/>
+                        <path
+                            d="M11.25 8.75H18.125L11.25 1.875V8.75ZM2.5 0H12.5L20 7.5V22.5C20 23.163 19.7366 23.7989 19.2678 24.2678C18.7989 24.7366 18.163 25 17.5 25H2.5C1.83696 25 1.20107 24.7366 0.732233 24.2678C0.263392 23.7989 0 23.163 0 22.5V2.5C0 1.1125 1.1125 0 2.5 0ZM12.5 22.5V21.25C12.5 19.5875 9.1625 18.75 7.5 18.75C5.8375 18.75 2.5 19.5875 2.5 21.25V22.5H12.5ZM7.5 12.5C6.83696 12.5 6.20107 12.7634 5.73223 13.2322C5.26339 13.7011 5 14.337 5 15C5 15.663 5.26339 16.2989 5.73223 16.7678C6.20107 17.2366 6.83696 17.5 7.5 17.5C8.16304 17.5 8.79893 17.2366 9.26777 16.7678C9.73661 16.2989 10 15.663 10 15C10 14.337 9.73661 13.7011 9.26777 13.2322C8.79893 12.7634 8.16304 12.5 7.5 12.5Z"
+                            fill="#00AAFF" />
                     </g>
                     <defs>
-                    <clipPath id="clip0_1594_1042">
-                    <rect width="20" height="25" fill="white"/>
-                    </clipPath>
+                        <clipPath id="clip0_1594_1042">
+                            <rect width="20" height="25" fill="white" />
+                        </clipPath>
                     </defs>
                 </svg>
 
                 Upload Your Resume
             </button>
-                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
+
+            {{-- Modal upload --}}
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered custom-modal-height custom-modal-width"
+                    style="max-width: 750px;">
+                    <div class="modal-content">
                         <div class="modal-body">
-                          ...
+                            <div class="container">
+                                <div class="close-btn" data-bs-dismiss="modal" aria-label="Close">×</div>
+                                <h1 style="margin-top: 12px;">Add your resume</h1>
+                                <p>Use a PDF, Word doc, or rich text file – make sure it’s 5MB or less.</p>
+                                <div class="upload-box" id="uploadBox">
+                                    <x-svg class="vector-icon">{{ asset('assets/svg/login/modal.svg') }}</x-svg>
+                                    <p style="margin-left: 195px;">Drag and drop or <a href="#"
+                                            id="chooseFileLink">choose file</a></p>
+                                    <input type="file" id="fileInput" style="display: none;"
+                                        accept=".pdf, .doc, .docx, .rtf" />
+                                </div>
+                            </div>
                         </div>
                         <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Understood</button>
+                            <button type="button" class="btn btn-primary"
+                                style="background-color: #00AAFF; margin-right: 20px;">Continue</button>
                         </div>
-                      </div>
                     </div>
-                  </div>
-            <button class="btn btn-outline-custom my-3">
-                Fill in manually
-            </button>
+                </div>
+            </div>
         </div>
+
+        <button class="btn btn-outline-custom my-3">
+            Fill in manually
+        </button>
+    </div>
     </div>
 
 
@@ -303,5 +387,16 @@
                 }
             });
         }
+    </script>
+
+    <script>
+        document.getElementById('uploadBox').addEventListener('click', function() {
+            document.getElementById('fileInput').click();
+        });
+
+        document.getElementById('chooseFileLink').addEventListener('click', function(e) {
+            e.preventDefault(); // Mencegah default behavior link
+            document.getElementById('fileInput').click();
+        });
     </script>
 @endsection
